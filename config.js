e1e40f61e8da3d5dc06c161a751600ed5fd7f9a0
Surfie <o.rickett@gmail.com>
2026-05-06 20:12:17 +0200

    Add Supabase anon key to config

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // "anon public" key and paste it below in place of PASTE_ANON_KEY_HERE.
 
 export const SUPABASE_URL = "https://xmqlbsouvpltxntdvfss.supabase.co";
-export const SUPABASE_ANON_KEY = "PASTE_ANON_KEY_HERE";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtcWxic291dnBsdHhudGR2ZnNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNzcxMzUsImV4cCI6MjA5MzY1MzEzNX0.f0t-CMHw8tInLTZjoWr_HMw4c_-qvafaLtgcnAjPI20";
